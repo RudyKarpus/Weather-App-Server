2847a40f95c420496839176fac522fc8af19a397
Szymon Kaminski <szymon.kaminkk@gmail.com>
2025-06-29 12:53:16 +0200

    feat: added lock on caching to avoid race conditions

--- weather_app/services.py
+++ weather_app/services.py
@@ -1,9 +1,22 @@
+import time as pytime
+from contextlib import contextmanager
 from datetime import datetime, time
 
 import requests
 from django.core.cache import cache
 
 from .utils.time_utils import get_timezone, get_timezone_time
+
+
+@contextmanager
+def cache_lock(key, timeout=5):
+    lock_id = f"{key}-lock"
+    acquired = cache.add(lock_id, "locked", timeout=timeout)
+    try:
+        yield acquired
+    finally:
+        if acquired:
+            cache.delete(lock_id)
 
 
 class WeatherService:
@@ -27,32 +40,39 @@ class WeatherService:
         data = cache.get(cache_key)
 
         if not data:
-            url = "https://api.open-meteo.com/v1/forecast"
-            time_zone = get_timezone(latitude, longitude)
-            params = {
-                "latitude": latitude,
-                "longitude": longitude,
-                "daily": [
-                    "temperature_2m_max",
-                    "temperature_2m_min",
-                    "weathercode",
-                    "sunshine_duration",
-                    "surface_pressure_mean",
-                ],
-                "timezone": time_zone,
-            }
-            try:
-                response = requests.get(url, params=params, timeout=10)
-                data = response.json().get("daily", {})
-            except requests.RequestException as e:
-                print("WeatherService: Couldn't get data from api", e)
-                return None
-            # Calculate cache time
-            tz_time = get_timezone_time(time_zone)
-            midnight = datetime.combine(
-                tz_time.date(), time(23, 59, 59, 9), tzinfo=tz_time.tzinfo
-            )
-            seconds_to_midnight = int((midnight - tz_time).total_seconds())
+            with cache_lock(cache_key, timeout=10) as acquired:
+                if acquired:
+                    data = cache.get(cache_key)
+                    if not data:
+                        url = "https://api.open-meteo.com/v1/forecast"
+                        time_zone = get_timezone(latitude, longitude)
+                        params = {
+                            "latitude": latitude,
+                            "longitude": longitude,
+                            "daily": [
+                                "temperature_2m_max",
+                                "temperature_2m_min",
+                                "weathercode",
+                                "sunshine_duration",
+                                "surface_pressure_mean",
+                            ],
+                            "timezone": time_zone,
+                        }
+                        try:
+                            response = requests.get(url, params=params, timeout=10)
+                            data = response.json().get("daily", {})
+                        except requests.RequestException as e:
+                            print("WeatherService: Couldn't get data from api", e)
+                            return None
 
-            cache.set(cache_key, data, seconds_to_midnight)
+                        tz_time = get_timezone_time(time_zone)
+                        midnight = datetime.combine(
+                            tz_time.date(), time(23, 59, 59, 9), tzinfo=tz_time.tzinfo
+                        )
+                        seconds_to_midnight = int((midnight - tz_time).total_seconds())
+
+                        cache.set(cache_key, data, seconds_to_midnight)
+                else:
+                    pytime.sleep(1)
+                    data = cache.get(cache_key)
         return data
